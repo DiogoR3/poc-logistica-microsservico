08b08f338a52c52939a044948c800af9f6563239
Diogo Rodrigues <diogo.rodcarreira@hotmail.com>
2021-07-24 15:37:04 -0300

    Formulario de login

--- src/POC.LogisticaMicrosservico.Web/ClientApp/src/api/index.ts
+++ src/POC.LogisticaMicrosservico.Web/ClientApp/src/api/index.ts
@@ -1,22 +1,19 @@
 import axios, { AxiosResponse } from 'axios'
 
-const EndpointInformacoesCadastrais = process.env.HOSTNAME_INFORMACOESCADASTRAIS
-const EndpointServicosAoCliente = process.env.HOSTNAME_SERVICOSAOCLIENTE
-
 function Login(login: string, senha: string) : Promise<AxiosResponse>{
-    return axios.post(EndpointInformacoesCadastrais + '/Login', { login: login, senha: senha })
+    return axios.post('/api/Login', { login: login, senha: senha })
 }
 
 function ListarUsuarios() : Promise<AxiosResponse>{
-    return axios.get(EndpointInformacoesCadastrais + '/Usuario')
+    return axios.get('/api/Usuario')
 }
 
 function Atendimento(id: number) : Promise<AxiosResponse>{
-    return axios.get(EndpointServicosAoCliente + '/Atendimento/' + id)
+    return axios.get('/api/Atendimento/' + id)
 }
 
 function Mercadoria(id: number) : Promise<AxiosResponse>{
-    return axios.get(EndpointServicosAoCliente + '/Mercadoria/' + id)
+    return axios.get('/api/Mercadoria/' + id)
 }
 
 export default {
--- src/POC.LogisticaMicrosservico.Web/ClientApp/src/components/CaixaLogin.vue
+++ src/POC.LogisticaMicrosservico.Web/ClientApp/src/components/CaixaLogin.vue
@@ -1,21 +1,89 @@
 <template>
-  <v-container>
-    <v-text-field
-    color="success"
-    loading
-    disabled
-  ></v-text-field>
-  </v-container>
+  <v-card
+    elevation="4"
+    :loading="carregando"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="primary"
+        height="5"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img height="200" src="../assets/boa-entrega.png"></v-img>
+
+    <v-card-title>Boa Entrega</v-card-title>
+
+    <v-form v-model="valido">
+      <v-card-text>
+        <v-text-field
+          v-model="login"
+          :counter="10"
+          label="Login"
+          :rules="regras"
+          required
+          max-length="10"
+          autocomplete="on"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="senha"
+          :counter="10"
+          label="Senha"
+          :rules="regras"
+          required
+          type="password"
+          autocomplete="on"
+        ></v-text-field>
+
+        <v-layout justify-center>
+          <v-btn
+            color="primary"
+            elevation="2"
+            @click="logar"
+            :disabled="!valido"
+          >
+            Login
+          </v-btn>
+        </v-layout>
+      </v-card-text>
+    </v-form>
+  </v-card>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
+import Vue from "vue";
+import api from '../api'
 
-  export default Vue.extend({
-    name: 'CaixaLogin',
+export default Vue.extend({
+  name: "CaixaLogin",
 
-    data: () => ({
-      
-    }),
-  })
+  data: () => ({
+    login: '' as string,
+    senha: '' as string,
+    carregando: false as boolean,
+    valido: true as boolean,
+    regras:[
+      (v: string) => !!v || "Campo obrigatório",
+      (v: string) => v.length <= 10 || "Campo deve ter menos de 10 caracteres"
+    ]
+  }),
+  methods: {
+    async logar() {
+      this.carregando = true
+
+      try{
+        let resultado = await api.Login(this.login, this.senha);
+        console.log(resultado)
+      }catch(ex){
+        console.log(ex)
+      }finally{
+        this.carregando = false
+      }
+    },
+  },
+});
 </script>
